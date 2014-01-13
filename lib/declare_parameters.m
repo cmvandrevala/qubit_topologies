06050d5824df7b6d1e651ad58a55e8dc57ae40d1
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-01-12 21:18:20 -0500

    Set default inputs

--- lib/declare_parameters.m
+++ lib/declare_parameters.m
@@ -1,6 +1,6 @@
 boltzmans_constant = 1;
-cells_on_side_of_grid = 20;
+cells_on_side_of_grid = 50;
 interaction_strength = 1;
 maximum_temperature = 5;
-number_of_potential_spin_flips = 10;
-number_of_random_temperatures_to_test = 100;
+number_of_potential_spin_flips = 2500;
+number_of_random_temperatures_to_test = 200;
